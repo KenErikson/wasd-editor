9013d6e7672e86f9d41534aaa76a6cf159298b70
Kn <kn@example.com>
2025-08-06 22:31:07 +0300

    Editor progress

--- wasd.go
+++ wasd.go
@@ -1,12 +1,34 @@
 package main
 
 import (
+	"github.com/gdamore/tcell/v2"
 	"github.com/rivo/tview"
 )
 
 func main() {
-	box := tview.NewBox().SetBorder(true).SetTitle("Hello, world!")
-	if err := tview.NewApplication().SetRoot(box, true).Run(); err != nil {
+	app := tview.NewApplication()
+	textView := tview.NewTextView()
+	textView.SetBackgroundColor(tcell.NewRGBColor(30, 29, 30))
+	textView.SetText("AAAAAAAAAAAAAAAA\nBBBBBBBBBBBBBB\nCCCCCC\nA\nBBBBBBBBBBBBBB\nCCCCCCA\nBBBBBBBBBBBBBB\nCCCCCCA\nBBBBBBBBBBBBBB\nCCCCCCA\nBBBBBBBBBBBBBB\nCCCCCCA\nBBBBBBBBBBBBBB\nCCCCCCA\nBBBBBBBBBBBBBB\nCCCCCCA\nBBBBBBBBBBBBBB\nCCCCCCA\nBBBBBBBBBBBBBB\nCCCCCCA\nBBBBBBBBBBBBBB\nCCCCCCA\nBBBBBBBBBBBBBB\nCCCCCCA\nBBBBBBBBBBBBBB\nCCCCCCA\nBBBBBBBBBBBBBB\nCCCCCCA\nBBBBBBBBBBBBBB\nCCCCCCA\nBBBBBBBBBBBBBB\nCCCCCCA\nBBBBBBBBBBBBBB\nCCCCCCA\nBBBBBBBBBBBBBB\nCCCCCCA\nBBBBBBBBBBBBBB\nCCCCCCA\nBBBBBBBBBBBBBB\nCCCCCCA\nBBBBBBBBBBBBBB\nCCCCCCA\nBBBBBBBBBBBBBB\nCCCCCCA\nBBBBBBBBBBBBBB\nCCCCCCA\nBBBBBBBBBBBBBB\nCCCCCCA\nBBBBBBBBBBBBBB\nCCCCCCA\nBBBBBBBBBBBBBB\nCCCCCCA\nBBBBBBBBBBBBBB\nCCCCCCA\nBBBBBBBBBBBBBB\nCCCCCCA\nBBBBBBBBBBBBBB\nCCCCCCA\nBBBBBBBBBBBBBB\nCCCCCCA\nBBBBBBBBBBBBBB\nCCCCCCA\nBBBBBBBBBBBBBB\nCCCCCCA\nBBBBBBBBBBBBBB\nCCCCCCA\nBBBBBBBBBBBBBB\nCCCCCCA\nBBBBBBBBBBBBBB\nCCCCCCA\nBBBBBBBBBBBBBB\nCCCCCCA\nBBBBBBBBBBBBBB\nCCCCCCA\nBBBBBBBBBBBBBB\nCCCCCCA\nBBBBBBBBBBBBBB\nCCCCCCA\nBBBBBBBBBBBBBB\nCCCCCCA\nBBBBBBBBBBBBBB\nCCCCCCA\nBBBBBBBBBBBBBB\nCCCCCCA\nBBBBBBBBBBBBBB\nCCCCCCA\nBBBBBBBBBBBBBB\nCCCCCCA\nBBBBBBBBBBBBBB\nCCCCCCA\nBBBBBBBBBBBBBB\nCCCCCCA\nBBBBBBBBBBBBBB\nCCCCCCA\nBBBBBBBBBBBBBB\nCCCCCCA\nBBBBBBBBBBBBBB\nCCCCCCA\nBBBBBBBBBBBBBB\nCCCCCCA\nBBBBBBBBBBBBBB\nCCCCCCA\nBBBBBBBBBBBBBB\nCCCCCCA\nBBBBBBBBBBBBBB\nCCCCCCA\nBBBBBBBBBBBBBB\nCCCCCCA\nBBBBBBBBBBBBBB\nCCCCCCA\nBBBBBBBBBBBBBB\nCCCCCCA\nBBBBBBBBBBBBBB\nCCCCCCA\nBBBBBBBBBBBBBB\nCCCCCCA\nBBBBBBBBBBBBBB\nCCCCCCA\nBBBBBBBBBBBBBB\nCCCCCCA\nBBBBBBBBBBBBBB\nCCCCCCA\nBBBBBBBBBBBBBB\nCCCCCCA\nBBBBBBBBBBBBBB\nCCCCCCA\nBBBBBBBBBBBBBB\nCCCCCCA\nBBBBBBBBBBBBBB\nCCCCCCA\nBBBBBBBBBBBBBB\nCCCCCCA\nBBBBBBBBBBBBBB\nCCCCCC")
+	textView.SetTextColor(tcell.NewRGBColor(118, 189, 217))
+
+	rowNumbers := tview.NewTextView()
+	rowNumbers.SetTextAlign(tview.AlignCenter).SetText("~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n~\n")
+	rowNumbers.SetBackgroundColor(tcell.NewRGBColor(30, 29, 30))
+	rowNumbers.SetTextColor(tcell.NewRGBColor(128, 128, 128))
+
+	statusBar := tview.NewTextView()
+	statusBar.SetBackgroundColor(tcell.NewRGBColor(36, 35, 36))
+	statusBar.SetTextColor(tcell.NewRGBColor(212, 212, 212))
+	statusBar.SetText("Status: Fire").SetTextAlign(tview.AlignRight)
+
+	gridView := tview.NewGrid().SetColumns(2, 0).SetRows(0, 1)
+	gridView.SetBorderPadding(0, 0, 0, 0)
+	gridView.AddItem(rowNumbers, 0, 0, 1, 1, 0, 0, false)
+	gridView.AddItem(textView, 0, 1, 1, 1, 0, 0, true)
+	gridView.AddItem(statusBar, 1, 0, 1, 2, 0, 0, false)
+
+	if err := app.SetRoot(gridView, true).SetFocus(gridView).Run(); err != nil {
 		panic(err)
 	}
 }
